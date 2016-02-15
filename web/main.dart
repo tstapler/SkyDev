23e1e9e1af2a16e61de6c9881440a3c947e4aaf1
ssrirama <ssrirama@iastate.edu>
2016-02-15 01:05:34 -0600

    Working on implementing sorting (from scratch)

--- web/main.dart
+++ web/main.dart
@@ -14,6 +14,7 @@ void main() {
   toDoInput = querySelector('#to-do-input');
   toDoList = querySelector('#to-do-list');
   toDoInput.onChange.listen(addToDoItem);
+  toDoInput.onChange.listen(sortList);
 
   deleteAll = querySelector("#delete-all");
   deleteAll.onClick.listen(deleteAllChildren);
@@ -28,5 +29,17 @@ void addToDoItem(Event e) {
   newToDo.text = toDoInput.value;
   newToDo.onClick.listen((e) => newToDo.remove());
   toDoInput.value = '';
+  toDoList.children.add(newToDo);
+}
+
+void sortList(Event e){
+  List<LIElement> list = toDoList.children;
+  for(int i = 0; i < list.length; i++){
+    list[i].text = "lol";
+    toDoList.children.add(list[i]);
+  }
+  var newToDo = new LIElement();
+  newToDo.text = "haha";
+  newToDo.onClick.listen((e) => newToDo.remove());
   toDoList.children.add(newToDo);
 }
