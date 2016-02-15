227ed83a42239ac6144865e199cdcdf84ed8404e
Chris Stapler <cstapler@iastate.edu>
2016-02-15 01:00:06 -0600

    Introduces group page experiment code

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,17 @@
 
 import 'dart:html';
 
+UListElement emailList;
+InputElement textBox;
 void main() {
-  querySelector('#output').text = 'Dart is fun!';
+  querySelector('#heading1').text = 'Group Page!!!';
+  textBox = querySelector('#input_field');
+  emailList = querySelector('#email_list');
+  textBox.onChange.listen(addEmail);
+}
+void addEmail(Event e){
+  var nextEmail = new LIElement();
+  nextEmail.text = textBox.value;
+  textBox.value = '';
+  emailList.children.add(nextEmail);
 }
