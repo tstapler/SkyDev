3d34f63146c8051d6616506f3b9303b3ac219d2c
ssrirama <ssrirama@iastate.edu>
2016-02-19 17:00:59 -0600

    Getting rid of websocket stuff to start over. Probably going to use a different framework

--- web/client.dart
+++ web/client.dart
@@ -9,45 +9,10 @@ import 'package:react/react_client.dart' as reactClient;
 import 'package:react/react.dart';
 import 'dart:async';
 
-WebSocket ws;
-
 void main() {
   reactClient.setClientConfiguration();
   var component = div({}, "SkyDev");
   render(component, querySelector('#content'));
-
-  initWebSocket();
-}
-
-void initWebSocket([int retrySeconds = 2]) {
-  var reconnectScheduled = false;
-
-  outputMsg("Connecting...");
-
-  ws = new WebSocket('ws://echo.websocket.org');
-
-  void scheduleReconnect() {
-    if (!reconnectScheduled) {
-      new Timer(new Duration(milliseconds: 1000 * retrySeconds), () => initWebSocket(retrySeconds * 2));
-    }
-    reconnectScheduled = true;
-  }
-
-  ws.onOpen.listen((e) {
-    ws.send('Hello from Dart!');
-  });
-
-  ws.onClose.listen((e) {
-    scheduleReconnect();
-  });
-
-  ws.onError.listen((e) {
-    scheduleReconnect();
-  });
-
-  ws.onMessage.listen((MessageEvent e) {
-    outputMsg('${e.data}');
-  });
 }
 
 outputMsg(String msg) {
