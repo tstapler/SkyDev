7bee9a8ef5ea627dd1fdd194e446143ca7fcf643
ssrirama <ssrirama@iastate.edu>
2016-02-19 19:44:57 -0600

    Websockets can connect and server can send messages to clients. Still working on getting clients to send messages back to the server

--- server.dart
+++ server.dart
@@ -4,7 +4,6 @@ import 'package:http_server/http_server.dart';
 main() async {
 	var requestServer = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8081);
 	print('listening on localhost, port ${requestServer.port}');
-
 	await for (HttpRequest request in requestServer) {
 		final String _buildPath = Platform.script.resolve('build/web/').toFilePath();
 		final VirtualDirectory _clientDir = new VirtualDirectory(_buildPath);
@@ -12,11 +11,16 @@ main() async {
 		    request.response.redirect(Uri.parse('index.html'));
 		} else if (request.uri.path == '/ws') {
 	    	// Upgrade an HttpRequest to a WebSocket connection.
-	    	var socket = await WebSocketTransformer.upgrade(request);
+	    	WebSocket socket = await WebSocketTransformer.upgrade(request);
 	    	socket.listen(handleMsg);
+	    	// socket.add('Connected to server');
 	  	} else {
 	    	var fileUri = new Uri.file(_buildPath).resolve(request.uri.path.substring(1));
 	    	_clientDir.serveFile(new File(fileUri.toFilePath()), request);
 		}
 	}
+}
+
+handleMsg(msg) {
+	print('Message received: $msg');
 }
--- web/client.dart
+++ web/client.dart
@@ -9,17 +9,31 @@ import 'package:react/react_client.dart' as reactClient;
 import 'package:react/react.dart';
 import 'dart:async';
 
-void main() {
-  reactClient.setClientConfiguration();
-  var component = div({}, "SkyDev");
-  render(component, querySelector('#content'));
+void main() async {
+	WebSocket ws = new WebSocket('ws://localhost:8081/ws');
+
+	ws.onOpen.listen((event){
+		outputMsg('Socket connection opened');
+	});
+
+	ws.onMessage.listen((event){
+		if(event.data == 'Connected to server'){
+			outputMsg('Connected to server');
+		} else {
+			outputMsg(event.data);
+		}
+	});
+
+	reactClient.setClientConfiguration();
+	var component = div({}, "SkyDev");
+	render(component, querySelector('#content'));
 }
 
 outputMsg(String msg) {
-  var output = querySelector('#output');
-  var text = msg;
-  if (!output.text.isEmpty) {
-    text = "${output.text}\n${text}";
-  }
-  output.text = text;
+	var output = querySelector('#output');
+	var text = msg;
+	if (!output.text.isEmpty) {
+		text = "${output.text}\n${text}";
+	}
+	output.text = text;
 }
