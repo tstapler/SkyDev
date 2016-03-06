22c94864cca10afd80369147bdd6a45b6e1a6684
ssrirama <ssrirama@iastate.edu>
2016-03-05 18:44:21 -0600

    Fixing the savagery that is using spaces instead of tabs

--- server.dart
+++ server.dart
@@ -7,7 +7,6 @@ import 'package:trestle/gateway.dart';
 import 'package:query_string/query_string.dart';
 
 WebSocket socket;
-<<<<<<< HEAD
 List<WebSocket> list;
 
 main() async {
@@ -18,72 +17,41 @@ print('listening on http://${requestServer.address.host}:${requestServer.port}')
 	await for (HttpRequest request in requestServer) {
 		final String _buildPath = Platform.script.resolve('build/web/').toFilePath();
 		final VirtualDirectory _clientDir = new VirtualDirectory(_buildPath);
-	  if (request.uri.path == '/') {
+		if (request.uri.path == '/') {
 		    request.response.redirect(Uri.parse('index.html'));
 		} else if (request.uri.path == '/ws') {
-	    	// Upgrade an HttpRequest to a WebSocket connection.
-	    	socket = await WebSocketTransformer.upgrade(request);
-	    	print('Server has gotten a websocket request');
-	    	list.add(socket);
-	    	socket.listen(handleMsg);
-	  } else if (request.uri.path == '/login'){
+			// Upgrade an HttpRequest to a WebSocket connection.
+			socket = await WebSocketTransformer.upgrade(request);
+			print('Server has gotten a websocket request');
+			list.add(socket);
+			socket.listen(handleMsg);
+		} else if (request.uri.path == '/login'){
 			switch (request.method) {
-	      case 'POST':
-	        handleLogin(request);
-	        break;
-	      case 'OPTIONS':
-	        handleOptions(request);
-	        break;
-	      default:
+				case 'POST':
+					handleLogin(request);
+				break;
+				case 'OPTIONS':
+					handleOptions(request);
+				break;
+				default:
 					request.response.redirect(Uri.parse('login.html'));
-	    }
-=======
-
-main() async {
-  await db_gateway.connect();
-  var requestServer = await HttpServer.bind(InternetAddress.ANY_IP_V4, 8081);
-  print(
-      'listening on http://${requestServer.address.host}:${requestServer.port}');
-  await for (HttpRequest request in requestServer) {
-    final String _buildPath =
-        Platform.script.resolve('build/web/').toFilePath();
-    final VirtualDirectory _clientDir = new VirtualDirectory(_buildPath);
-    if (request.uri.path == '/') {
-      request.response.redirect(Uri.parse('index.html'));
-    } else if (request.uri.path == '/ws') {
-      // Upgrade an HttpRequest to a WebSocket connection.
-      socket = await WebSocketTransformer.upgrade(request);
-      print('Server has gotten a websocket request');
-      socket.listen(handleMsg);
-    } else if (request.uri.path == '/login') {
-      switch (request.method) {
-        case 'POST':
-          handleLogin(request);
-          break;
-        case 'OPTIONS':
-          handleOptions(request);
-          break;
-        default:
-          request.response.redirect(Uri.parse('login3.html'));
-      }
->>>>>>> master
-    } else if (request.uri.path == '/viewdb') {
-      handleView(request);
-    } else {
-      var fileUri =
-          new Uri.file(_buildPath).resolve(request.uri.path.substring(1));
-      _clientDir.serveFile(new File(fileUri.toFilePath()), request);
-    }
-  }
-  await db_gateway.disconnect();
+			}
+		} else if (request.uri.path == '/viewdb') {
+			handleView(request);
+		} else {
+			var fileUri = new Uri.file(_buildPath).resolve(request.uri.path.substring(1));
+			_clientDir.serveFile(new File(fileUri.toFilePath()), request);
+		}
+	}
+	await db_gateway.disconnect();
 }
 
 Future handleLogin(HttpRequest req) async {
-  HttpResponse res = req.response;
-  print('${req.method}: ${req.uri.path}');
-  addCorsHeaders(res);
-  var jsonString = await req.transform(UTF8.decoder).join();
-  Map jsonData = QueryString.parse(jsonString);
+	HttpResponse res = req.response;
+	print('${req.method}: ${req.uri.path}');
+	addCorsHeaders(res);
+	var jsonString = await req.transform(UTF8.decoder).join();
+	Map jsonData = QueryString.parse(jsonString);
 	if(await verifyUser(jsonData)){
 		res.write('Success');
 		res.close();
@@ -95,81 +63,52 @@ Future handleLogin(HttpRequest req) async {
 }
 
 void addCorsHeaders(HttpResponse res) {
-  res.headers.add('Access-Control-Allow-Origin', '*');
-  res.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.headers.add('Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept');
+	res.headers.add('Access-Control-Allow-Origin', '*');
+	res.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS');
+	res.headers.add('Access-Control-Allow-Headers',
+	'Origin, X-Requested-With, Content-Type, Accept');
 }
 
 void handleOptions(HttpRequest req) {
-  HttpResponse res = req.response;
-  addCorsHeaders(res);
-  print('${req.method}: ${req.uri.path}');
-  res.statusCode = HttpStatus.NO_CONTENT;
-  res.close();
+	HttpResponse res = req.response;
+	addCorsHeaders(res);
+	print('${req.method}: ${req.uri.path}');
+	res.statusCode = HttpStatus.NO_CONTENT;
+	res.close();
 }
 
 void defaultHandler(HttpRequest req) {
-  HttpResponse res = req.response;
-  addCorsHeaders(res);
-  res.statusCode = HttpStatus.NOT_FOUND;
-  res.write('Not found: ${req.method}, ${req.uri.path}');
-  res.close();
+	HttpResponse res = req.response;
+	addCorsHeaders(res);
+	res.statusCode = HttpStatus.NOT_FOUND;
+	res.write('Not found: ${req.method}, ${req.uri.path}');
+	res.close();
 }
 
 void handleMsg(String m) async {
-<<<<<<< HEAD
 	print('Message received: $m');
-	print('${list.length}');
-	
-	File f = new File("files/doc");
 
-	if(m.startsWith("Synchronize")){
-		if(!f.existsSync()){
-			socket.add("Error: Could not find file");
+	if (m.startsWith("Open:")) {
+		m = m.replaceFirst("Open:", "", 0);
+		File f = new File("files/$m");
+		if (!f.existsSync()) {
+		  socket.add("Error: Could not find file");
 		} else {
-			for(int i=0; i < list.length; i++){
-				String contents = f.readAsStringSync();
-				list[i].add("Contents:" + contents);
-			}
+		  f.readAsString().then((String contents) {
+		    socket.add("Contents:" + contents);
+		  });
 		}
-	} else if(m.startsWith("Save:")) {
+	} else if (m.startsWith("Save:")) {
 		// print(m);
 		m = m.replaceFirst("Save:", "", 0);
 		String filename = m.substring(0, m.indexOf(":"));
 		// print(filename);
 		m = m.replaceFirst("$filename:", "", 0);
 		// print(m.isEmpty);
-		f.writeAsStringSync(m);
-		for(int i=0; i < list.length; i++){
-			String contents = f.readAsStringSync();
-			list[i].add("Contents:" + contents);
-		}
+		(new File("files/$filename")).writeAsStringSync(m);
+
 		// print("Contents:\n\t" + (new File("files/$filename")).readAsStringSync());
-=======
-  print('Message received: $m');
-
-  if (m.startsWith("Open:")) {
-    m = m.replaceFirst("Open:", "", 0);
-    File f = new File("files/$m");
-    if (!f.existsSync()) {
-      socket.add("Error: Could not find file");
-    } else {
-      f.readAsString().then((String contents) {
-        socket.add("Contents:" + contents);
-      });
-    }
-  } else if (m.startsWith("Save:")) {
-    // print(m);
-    m = m.replaceFirst("Save:", "", 0);
-    String filename = m.substring(0, m.indexOf(":"));
-    // print(filename);
-    m = m.replaceFirst("$filename:", "", 0);
-    // print(m.isEmpty);
-    (new File("files/$filename")).writeAsStringSync(m);
-
-    // print("Contents:\n\t" + (new File("files/$filename")).readAsStringSync());
-  }
+	}
 }
 Future verifyUser(Map formData) async{
 	var uInput;
@@ -181,7 +120,6 @@ Future verifyUser(Map formData) async{
 	if(check_password(pInput, uData.password)){
 		print("Passwords matched");
 		return true;
->>>>>>> master
 	}
 	else{
 		print("Incorrect Password");
@@ -196,10 +134,10 @@ void createUser(Map formData){
 }
 
 void handleView(HttpRequest req) async {
-  HttpResponse res = req.response;
-  var user_list = await users.all().toList();
-  res.write(user_list);
-  res.close();
+	HttpResponse res = req.response;
+	var user_list = await users.all().toList();
+	res.write(user_list);
+	res.close();
 }
 
 void printError(error) => print(error);
