baab4cca3f6bff262bee43b81af955b7b781e7ff
cstapler <cstapler@iastate.edu>
2016-04-13 10:27:08 -0500

    Creates file for HandleRegistration and related for server use

---
+++ lib/serverHandleRegistration.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+import 'package:skydev/database.dart';
+import 'package:query_string/query_string.dart';
+
+Future handleRegister(HttpRequest req) async {
+  HttpResponse res = req.response;
+  print('${req.method}: ${req.uri.path}');
+  addCorsHeaders(res);
+  var queryString = await req.transform(UTF8.decoder).join();
+  Map queryData = QueryString.parse(queryString);
+  await createUser(queryData);
+  res.write('Success');
+  res.close();
+}
+
+Future createUser(Map formData) async{
+	var models = [new User.create(formData['username'],
+									formData['email'],
+										hash_password(formData['password']))];
+  await users.saveAll(models);
+}
+
+void addCorsHeaders(HttpResponse res) {
+	res.headers.add('Access-Control-Allow-Origin', '*');
+	res.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS');
+	res.headers.add('Access-Control-Allow-Headers',
+	'Origin, X-Requested-With, Content-Type, Accept');
+}
