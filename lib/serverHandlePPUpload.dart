5d5763cf19065e69902ba2af37a82a047e331639
cstapler <cstapler@iastate.edu>
2016-04-24 22:50:09 -0500

    Adds backend Handling of Profile Picture Uploading

--- lib/serverHandlePPUpload.dart
+++ lib/serverHandlePPUpload.dart
@@ -1,37 +1,29 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:skydev/database.dart';
+import 'package:image/image.dart';
 
-void handlePPUpload(HttpRequest req){
-  _readBody(req, (body) {
-
-        // handle your dataURL
-        // example with image : data:image/jpeg;base64,/9j/4S2YRXhpZgAATU0AK...
-
-        // return result
-        HttpResponse res = req.response;
-        res.statusCode = HttpStatus.CREATED;
-        res.contentLength = 0;
-        res.close();
-      });
-}
-
-/// Read body of [request] and call [handleBody] when complete.
-_readBody(HttpRequest request, void handleBody(String body)) {
-  String bodyString = ""; // request body byte data
-  final completer = new Completer();
-  final sis = new StringInputStream(request.inputStream, Encoding.UTF_8);
-  sis.onData = (){
-    bodyString = bodyString.concat(sis.read());
-  };
-  sis.onClosed = () {
-    completer.complete("");
-  };
-  sis.onError = (Exception e) {
-    print('exception occured : ${e.toString()}');
-  };
-  // process the request and send a response
-  completer.future.then((_){
-    handleBody(bodyString);
-  });
+handlePPUpload(HttpRequest req) async {
+      print(req.toString());
+      Cookie cookie;
+      HttpResponse res = req.response;
+      User user;
+      try {
+        cookie = req.cookies.singleWhere((element) => element.name == "SessionID");
+        user = await users.where((user) => user.sessionid == cookie.value).first();
+      } catch (e) {
+        print(e);
+        res.write(JSON.encode(null));
+      }
+      var reqString = await req.transform(UTF8.decoder).join();
+      //print(reqString);
+      //Image newImage = decodeImage(reqString.codeUnits);
+      (new File("web/profileImages/${user.username}.txt")).createSync(recursive: true);
+      File newFile = new File("web/profileImages/${user.username}.txt");
+      newFile.writeAsStringSync(reqString);
+      user.profilepicturename = "profileImages/${user.username}.jpg";
+      users.saveAll([user]);
+      res.statusCode = HttpStatus.CREATED;
+      res.contentLength = 0;
+      res.close();
 }
